fc3272b60ff174f8157a0f0d62e422208a580f96
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-01-30 15:52:29 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,48 @@
+console.log("Operators in Js")
+
+let a = 45
+let b = 4
+
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b)
+console.log("a % b = ", a%b)
+console.log("++a = ", ++a) //46
+console.log("a = ", a) //46
+console.log("a-- = ", a--) //46
+console.log("a-- = ", a) //45
+
+let c =10
+
+console.log("++c ", ++c) //11
+console.log("c++ ", c++) //11
+console.log("--c ", --c) //11
+console.log("c-- ", c--) //11
+console.log("c =", c) //10
+
+console.log("c-- ", c--) //10
+
+// Assignment Operators
+let assign =1
+
+assign +=5
+console. log("a is now = ", assign)
+assign -= 5 // same as assignment = assignment - 5
+console. log("a is now = ", assign)
+assign *= 5 // same as assignment = assignment * 5
+console. log("a is now = ", assign)
+assign /= 5 // same as assignment = assignment / 5
+console. log("a is now = ", assign)
+
+//Comparison Operators
+
+let comp1 = 6
+let comp2 = "6"
+
+console.log("comp1 == comp2 is ", comp1 == comp2);    // true 
+console.log("comp1 != comp2 is ", comp1 != comp2);    // false
+console.log("comp1 === comp2 is ", comp1 === comp2);  // false bcoz 1 is no and 2 is string
+console.log("comp1 !== comp2 is ", comp1 !== comp2);  // true
+
+
