3edcc01beda810dee26731eeeea60385c4d8355f
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-01-31 22:23:56 +0530

    string

---
+++ strings.js
@@ -0,0 +1,30 @@
+
+
+//Template literals
+
+let boy1 ="harry"
+let boy2 ="nikhil"
+
+let sentence = `"${boy1}" is a friend of "${boy2}"`;
+
+console.log(sentence)
+
+//Escape Sequence character
+
+let fruit ="app\'le"
+console.log(fruit)
+
+console.log(fruit.length)
+
+let n ="har\nry"  //for new line
+
+console.log(n)
+
+console.log(boy1.toUpperCase())
+console.log(boy1.toLowerCase())
+console.log(boy1.slice(1,3))
+console.log(boy1.slice(3))
+console.log(boy1.concat(" is a friend of "+ boy2))
+
+let m = "         aman    "
+console.log(m.trim())
