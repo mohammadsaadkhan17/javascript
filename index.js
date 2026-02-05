891e4f9837b7f5bc85735207125c992e370dc83e
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-02-05 14:45:03 +0530

    index

---
+++ index.js
@@ -0,0 +1,13 @@
+var a =10
+let b ="Hey there"
+
+const n ="java"
+// n ="script" it will throw an error bcoz u cannot assign to constant 
+// const a; u should initialize const when declaring otherwise it will throw an error
+var c =null
+var d =undefined
+{
+    let b ="inside block"
+    console.log(b)
+}
+console.log(b)
