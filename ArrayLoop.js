1e21bdeaa91d3eb156e29f0473c01d73365c1bbe
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-02-07 17:50:49 +0530

    array loop

---
+++ ArrayLoop.js
@@ -0,0 +1,30 @@
+
+let no =[2,4,5,6,7,8,5,6]
+
+//For each loop
+no.forEach(e => {
+    console.log(e)
+});
+
+
+let n ="harry"
+
+//Array.from
+let a =Array.from(n)
+console.log(a)
+
+
+//For of
+
+for(let i of no){
+    console.log(i)
+}
+
+console.log("----------------------------------")
+//For in
+
+for(let i in no){
+    console.log(i)  //it will print key means the indexes
+}
+
+
