79693273784ecd54dace1950cf3696492e4f4421
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-02-20 23:38:26 +0530

    Add files via upload

---
+++ ps6.js
@@ -0,0 +1,45 @@
+// const canDrive = (age) => {
+
+//     return age >= 20 ? true : false
+
+// }
+
+// let runAgain = true;
+
+// while(runAgain) {
+
+//     let age =prompt("Enter the age:")
+
+//     age =Number.parseInt(age)
+
+//     if(age < 0){
+
+//         console.error("Age cannot be negative")
+
+//         break;
+//     }
+        
+// if(canDrive(age)){
+//     alert("Yes you can drive.")
+// }
+// else{
+
+//     alert("No you cannot drive!")
+// }
+  
+//     runAgain = confirm("Do you want to check again?")
+
+// }
+
+// let no =prompt("Enter a number:")
+
+// no =Number.parseInt(no)
+
+// if(no > 4){
+
+//     location.href = "https://google.com"
+// }
+
+let color =prompt("Enter the page background color")
+
+document.body.style.background = color
